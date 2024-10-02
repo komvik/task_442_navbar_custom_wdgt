ce8723cf0d4dc0c39b3dc998fd081d79be49fca8
Viktor <komvik.slack@gmail.com>
2024-10-02 11:11:57 +0200

    start task_442_navbar_custom_wdgt

---
+++ README.md
@@ -0,0 +1,3 @@
+# task_442_navbar_custom_wdgt
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/task_442_navbar_custom_wdgt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_442_navbar_custom_wdgt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
